85eacd2895d831ecd3f82be5f9f8672d87b7f739
Yokey <zyf6@outlook.com>
2022-03-31 09:32:29 +0800

    word count

--- RealTimeCompute/SparkDevelop/src/main/scala/Hello.scala
+++ RealTimeCompute/SparkDevelop/src/main/scala/Hello.scala
@@ -1,17 +1,31 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Hello {
+  var base_local_path: String = "C:/Users/zyf6/Documents/GitHub/CollegeCourse2022/RealTimeCompute/SparkDevelop"
+
+
+  def sparkWordCount(inputFile: String, outputFile: String) {
+    val conf = new SparkConf().setAppName("Word Count").setMaster("local")
+    val sc = new SparkContext(conf)
+    val input = sc.textFile(inputFile)
+    val words = input.flatMap(line => line.split(" "))
+    val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
+    counts.saveAsTextFile(outputFile)
+  }
+
 
   def sparkHello(name: String) = {
-    var conf = new SparkConf().setAppName("Hello").setMaster("local")
-    var sc = new SparkContext(conf)
-    var rdd = sc.parallelize(List("Hello", name))
+    val conf = new SparkConf().setAppName("Hello").setMaster("local")
+    val sc = new SparkContext(conf)
+    val rdd = sc.parallelize(List("Hello", name))
     rdd.collect().foreach(println)
   }
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
-    sparkHello("Yokey Xiao")
+//   sparkHello("Yokey Xiao")
+    sparkWordCount(base_local_path + "/src/main/resources/input.txt",
+      base_local_path + "/src/main/resources/output")
   }
 
 }
