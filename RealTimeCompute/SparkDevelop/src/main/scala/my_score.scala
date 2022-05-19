794d3b0446ba93904abc3d12f005c8dd1c6bc08b
Yokey <zyf6@outlook.com>
2022-05-19 21:11:23 +0800

    finish work

--- RealTimeCompute/SparkDevelop/src/main/scala/my_score.scala
+++ RealTimeCompute/SparkDevelop/src/main/scala/my_score.scala
@@ -1,21 +1,21 @@
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
 object my_score {
     def main(args: Array[String]): Unit = {
-      var conf=new SparkConf().setAppName("my_score").setMaster("local[*]")
 
-      var sc=new SparkContext(conf)
       val spark = SparkSession
         .builder()
-//        .appName("Score Statistics")
-//        .config("spark.master", "local")
+        .appName("Score Statistics")
+        .config("spark.master", "local")
         .getOrCreate()
 
-      val df = spark.read.option("header", "true").csv("C:/Users/zyf6/Documents/GitHub/CollegeCourse2022/DataVisualize/成绩/data.csv")
-      df.show()
+      val df = spark.read.option("header", "true").csv("../../DataVisualize/成绩/data.csv")
+      // df.show()
 
-//      spark.sql("SELECT * FROM global_temp.people").show()
-      df.filter("df('bfzcj')<60").show();
+      // df.filter("df('bfzcj')<60").show(); // Undefined function: 'df'
+
+      import spark.implicits._ // This import is needed to use the $-notation
+      df.filter($"bfzcj" < 60).show()
+      df.withColumn("bfzcj", $"bfzcj".cast("int")).sort($"bfzcj".desc).show()
     }
 }
