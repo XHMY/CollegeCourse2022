4c51b24f2d94c7627a4df440c1d239ca45a5b560
Yokey <zyf6@outlook.com>
2022-05-19 21:18:49 +0800

    修正描述

--- RealTimeCompute/SparkDevelop/src/main/scala/my_score.scala
+++ RealTimeCompute/SparkDevelop/src/main/scala/my_score.scala
@@ -9,11 +9,12 @@ object my_score {
         .config("spark.master", "local")
         .getOrCreate()
 
-      val df = spark.read.option("header", "true").csv("../../DataVisualize/成绩/data.csv")
+      val df = spark.read.option("header", "true").csv("../../DataVisualize/成绩/data.csv") // 此处需要修改为你的文件路径
       // df.show()
 
       // df.filter("df('bfzcj')<60").show(); // Undefined function: 'df'
 
+      // 列 'bfzcj' 为成绩，其它列都不重要
       import spark.implicits._ // This import is needed to use the $-notation
       df.filter($"bfzcj" < 60).show()
       df.withColumn("bfzcj", $"bfzcj".cast("int")).sort($"bfzcj".desc).show()
