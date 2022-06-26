97a1a096003c38e4beef2e507a785cd5e08450c8
Yokey <zyf6@outlook.com>
2022-06-26 21:21:50 +0800

    完善作业情况

--- FinalProject/Lei/RealTime/Education/src/main/java/FeedSteaming.java
+++ FinalProject/Lei/RealTime/Education/src/main/java/FeedSteaming.java
@@ -6,12 +6,12 @@ public class FeedSteaming {
     public static void main(String[] args) throws InterruptedException {
         // Write line to a new file
         String[] lines = {"Z2099009,坦克驾驶,91,通识课", "Z2099010,飞碟操纵,91,通识课", "Z2099011,火箭炮操纵,91,通识课", "Z2099012,机枪射击实战,99,专业课"};
-        Integer cnt = 0;
+        Integer cnt = 0; // 计数器
         for (String line : lines) {
             try {
-                FileWriter writer = new FileWriter("src/main/resources/file"+ cnt +".txt", false);
-                writer.write(line + "\n");
-                writer.close();
+                FileWriter writer = new FileWriter("src/main/resources/file"+ cnt +".txt", false); // 创建文件
+                writer.write(line + "\n"); // 写入文件
+                writer.close(); // 关闭文件
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- FinalProject/Lei/RealTime/Education/src/main/scala/streaming.scala
+++ FinalProject/Lei/RealTime/Education/src/main/scala/streaming.scala
@@ -22,7 +22,7 @@ object streaming {
     val ssc = new StreamingContext(conf, Seconds(3))
 
     val lines = ssc.socketTextStream("localhost", 9999) // 创建DStream，使用 socket 类型的数据源，本地端口号为 9999
-//    val lines = ssc.textFileStream("src/main/resources/") // 创建DStream，本地文件夹作为数据源
+//    val lines = ssc.textFileStream("src/main/resources/") // 创建DStream，本地文件夹作为数据源（使用 src/main/java/FeedSteaming.java 实时生成数据）
 
     lines.foreachRDD(rdd => {
       val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate() // 创建 SparkSession
